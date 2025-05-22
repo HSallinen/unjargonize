25bf320532c9261df348b1300eeaaf16b52eea16
mntdent <mntadent@gmail.com>
2025-05-22 16:38:29 +0300

    more work on the backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
 use backend::ThreadPool;
+use rusqlite::{Connection, Result, params};
 use std::{
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
@@ -18,15 +19,35 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let path = "/home/jjokinen/Documents/hackaton/thingymajig.sql";
+    let mut buf_reader = BufReader::new(&stream);
+    let mut line = String::new();
+
+    loop {
+        println!("{line}");
+        line.clear();
+        buf_reader
+            .read_line(&mut line)
+            .expect("Error in line reading");
+        if line == "" {
+            break;
+        }
+        if line.trim().is_empty() {
+            break;
+        }
+    }
+
+    line.clear();    
+    buf_reader
+        .read_line(&mut line)
+        .expect("Error in line reading");
+    println!("Request: {line:#?}");
+
+    let db = Connection::open(path).expect("DB connection failed");
+
+    println!("{}", db.is_autocommit());
 
     let response = "HTTP/1.1 200 OK\r\n\r\n";
 
-    println!("Request: {http_request:#?}");
     stream.write_all(response.as_bytes()).unwrap();
 }
